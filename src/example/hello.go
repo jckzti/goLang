dfd9b8c48abce9fa299ebae08b256ab04fba9096
Jonathan Cani <jonathan.kuntz@philips.com>
2019-10-08 12:03:07 -0300

    new test

--- src/example/hello.go
+++ src/example/hello.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello GoLang")
+	x := 5
+
+	if x > 6 {
+		fmt.Println("Print more than 6")
+	} else if x < 2 {
+
+	} else {
+		a := [5]int{5, 4, 3, 2, 1}
+
+		b := []int{5, 4, 3, 2, 1}
+		b = append(b, 13)
+		a[2] = 7
+		fmt.Println(a[1] + b[5])
+	}
 }
