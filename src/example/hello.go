e2fff0875c82e6ff1b7adc8d278e46be00a37afe
Jonathan Cani <jonathan.kuntz@philips.com>
2019-10-11 13:44:11 -0300

    Create rest api

--- src/example/hello.go
+++ src/example/hello.go
@@ -1,22 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// "Person type" (tipo um objeto)
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+// função principal
 func main() {
-	x := 5
 
-	if x > 6 {
-		fmt.Println("Print more than 6")
-	} else if x < 2 {
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
 
-	} else {
-		a := [5]int{5, 4, 3, 2, 1}
+	router := mux.NewRouter()
+	router.HandleFunc("/contato", GetPeople).Methods("GET")
+	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/contato/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/contato/{id}", DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
 
-		b := []int{5, 4, 3, 2, 1}
-		b = append(b, 13)
-		a[2] = 7
-		fmt.Println(a[2] + b[5])
+}
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
 	}
 }
