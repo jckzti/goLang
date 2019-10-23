682820c505db509032550bd4be41c3f11f991e20
Jonathan Cani <jonathan.kuntz@philips.com>
2019-10-23 12:42:24 -0300

    Remove person methods and add delete method for delete field

--- src/example/hello.go
+++ src/example/hello.go
@@ -8,18 +8,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// "Person type" (tipo um objeto)
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
 type UserConnection struct {
 	Dsn      string `json:"dsn"`
 	User     string `json:"user"`
@@ -33,7 +21,6 @@ type Field struct {
 	Title string `json:"title"`
 }
 
-var people []Person
 var fields []Field
 var userConnection UserConnection
 
@@ -44,20 +31,14 @@ func main() {
 	userConnection.User = "root"
 	userConnection.Password = ""
 
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+	fields = append(fields, Field{Name: "nome", Type: "text", Title: "Nome"})
 
 	router := mux.NewRouter()
-	router.HandleFunc("/contato", GetPeople).Methods("GET")
-	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/contato/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/contato/{id}", DeletePerson).Methods("DELETE")
-
 	router.HandleFunc("/conn", GetConn).Methods("GET")
 	router.HandleFunc("/fields", GetFields).Methods("GET")
 	router.HandleFunc("/field/{name}", GetField).Methods("GET")
 	router.HandleFunc("/field", CreateField).Methods("POST")
+	router.HandleFunc("/field/{name}", DeleteField).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 
@@ -73,13 +54,15 @@ func CreateField(w http.ResponseWriter, r *http.Request) {
 
 	for i, item := range fields {
 		if item.Name == field.Name {
-			fields[i].Type = field.Type
-			fields[i].Value = field.Value
+			fields[i] = field
 			//json.NewEncoder(w).Encode("{ 'Response':'atualizado'")
 			//json.NewEncoder(w).Encode(fields[i])
 			json.NewEncoder(w).Encode(fields)
 			return
 		}
+	}
+	if field.Type == "" {
+		field.Type = "text"
 	}
 	fields = append(fields, field)
 	json.NewEncoder(w).Encode(fields)
@@ -100,37 +83,13 @@ func GetFields(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(fields)
 }
 
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-func GetPerson(w http.ResponseWriter, r *http.Request) {
+func DeleteField(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Person{})
-}
-
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var person Person
-	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
+	for index, item := range fields {
+		if item.Name == params["name"] {
+			fields = append(fields[:index], fields[index+1:]...)
 			break
 		}
-		json.NewEncoder(w).Encode(people)
+		json.NewEncoder(w).Encode(fields)
 	}
 }
