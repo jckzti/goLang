18422f93d7c281167748fa1c73fc6b2c6176d170
Jonathan Cani <jonathan.kuntz@philips.com>
2019-11-09 00:15:44 -0300

    add a face recognition fuction to project

--- src/service/main.go
+++ src/service/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/Kagami/go-face"
+	"github.com/gorilla/mux"
 	colle "github.com/servicego/src/service/collections"
 	conn "github.com/servicego/src/service/connection"
 	structs "github.com/servicego/src/service/model"
-
-	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/mgo.v2/bson"
 )
 
 //UserConnection usada para guardar os dados da conexão para uso posterior no PHP
@@ -23,9 +28,26 @@ import (
 var fields []structs.Field
 var userConnection structs.UserConnection
 var forms []structs.Form
+var rec *face.Recognizer
+
+const dataDir = "images"
+
+type Face struct {
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	PersonName string             `json:"personName"`
+	Descriptor face.Descriptor    `json:"descriptor"`
+}
+
+var faceMD Face
 
 //Função principal
 func main() {
+	var err error
+	rec, err = face.NewRecognizer(dataDir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	fmt.Println("start serviceGO")
 	userConnection.Dsn = "mysql:dbname=web;host=127.0.0.1"
 	userConnection.User = "root"
@@ -39,7 +61,7 @@ func main() {
 
 	createFieldRequests(router)
 	createFormRequests(router)
-
+	setupRoutes()
 	log.Fatal(http.ListenAndServe(":8000", router))
 
 }
@@ -206,4 +228,163 @@ func deleteField(response http.ResponseWriter, request *http.Request) {
 			return
 		}
 	}
+}
+
+func identificaPessoa(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File\n")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	fileBytes, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(w, "Successfully Uploaded File\n")
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	faces, err := rec.Recognize(fileBytes)
+	if err != nil || len(faces) < 1 {
+		fmt.Println("Não foi possível identificar uma face na imagem enviada: %v\n", err)
+		fmt.Fprintf(w, "Não foi possível identificar uma face na imagem enviada: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Println("Rostos encontrados na imagem: ====wwww %v\n", len(faces))
+	fmt.Fprintf(w, "Rostos encontrados na imagem: %v\n", len(faces))
+
+	//Preenche com todos os descripts de todos os rostos da base de dados
+	rostos, err := getFacesData()
+
+	var samples []face.Descriptor
+	var pessoas []int32
+
+	for i, f := range rostos {
+		//saveImageDescriptor(f.Descriptor)
+		samples = append(samples, f.Descriptor)
+		// Each face is unique on that image so goes to its own category.
+		pessoas = append(pessoas, int32(i))
+	}
+
+	rec.SetSamples(samples, pessoas)
+
+	if err != nil {
+		fmt.Fprintf(w, "Não achou rosto na imagem: %v", err)
+		return
+	}
+
+	fmt.Fprint(w, "Pessoa identificadas:\n")
+	for i, facex := range faces {
+		//personID := rec.ClassifyThreshold(facex.Descriptor, 0.1)
+		personID := rec.Classify(facex.Descriptor)
+		if personID < 0 {
+			fmt.Fprintf(w, "Pessoa não encontrada ou não cadastrada no sistema\n")
+			continue
+		}
+
+		fmt.Println(personID)
+		fmt.Println(rostos[personID].PersonName)
+		fmt.Fprintf(w, "[%v] - Pessoa encontrada: %v\n", i+1, rostos[personID].PersonName)
+
+	}
+	//saveImageDescriptor(faces[0].Descriptor, rostos[avengerID].PersonName) // caso tenha encontrado, cataloga imagem para fazer parte do acervo
+}
+
+func enviaPessoa(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+	r.ParseMultipartForm(10 << 20)
+	nome := r.FormValue("nome")
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	//tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//tempFile.Write(fileBytes)
+
+	faces, err := rec.Recognize(fileBytes)
+	if err != nil {
+		fmt.Fprintf(w, "Não foi possível identificar uma face na imagem enviada: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Rostos encontrados na imagem: %v\n", len(faces))
+
+	saveImageDescriptor(faces[0].Descriptor, nome)
+
+}
+
+func saveImageDescriptor(descriptor face.Descriptor, nome string) error {
+	collection := conn.GetConn().Database("recog").Collection("faces")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	faceMD.ID = primitive.NewObjectID()
+	faceMD.PersonName = nome
+	faceMD.Descriptor = descriptor
+	//result, err := collection.InsertOne(ctx, form)
+	_, err := collection.InsertOne(ctx, faceMD)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getFacesData() ([]Face, error) {
+	fmt.Println("")
+	var rostos []Face
+
+	collection := conn.GetConn().Database("recog").Collection("faces")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var rosto Face
+		cursor.Decode(&rosto)
+		rostos = append(rostos, rosto)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return rostos, nil
+}
+
+func setupRoutes() {
+	http.HandleFunc("/identifica", identificaPessoa)
+	http.HandleFunc("/envia", enviaPessoa)
+	http.ListenAndServe(":8080", nil)
 }
